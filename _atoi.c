03ac44037e3d93d754cd14870887179c4016472e
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2022-11-16 23:17:03 +0300

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,12 +1,10 @@
 #include "shell.h"
 
 /**
- * interactive - returns true,
-                 if shell is interactive mode
+ * interactive - returns true, if shell is interactive mode
  * @info: struct address
  *
- * Return: 1 if interactive mode,
-           0 otherwise
+ * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
