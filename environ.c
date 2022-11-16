aaf4cd9d74dbb2bf0d2868677447606c87bbedc0
Esther Elisha <112513171+3SSI3@users.noreply.github.com>
2022-11-16 23:49:53 +0300

    Complete

--- environ.c
+++ environ.c
@@ -35,8 +35,7 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
+ * _mysetenv - Initialize a new environment variable,or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
